f6308843edd778097b10964f90b4f935e4bcf6e5
Luke Cunningham <luke.russell.cunningham@gmail.com>
2023-11-25 23:41:21 +1100

    Added an item version of BestKeyword, and BestKeywordInventory and BestKeywordRoom

--- MudSharpCore/FutureProg/Functions/BuiltIn/BestKeywordFunction.cs
+++ MudSharpCore/FutureProg/Functions/BuiltIn/BestKeywordFunction.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using JetBrains.Annotations;
 using MudSharp.Character;
 using MudSharp.Framework;
 using MudSharp.FutureProg.Variables;
+using MudSharp.GameItems;
 
 namespace MudSharp.FutureProg.Functions.BuiltIn;
 
@@ -11,10 +13,12 @@ internal class BestKeywordFunction : BuiltInFunction
 {
 	private readonly IFuturemud _gameworld;
 
-	public BestKeywordFunction(IList<IFunction> parameters, IFuturemud gameworld)
+	public BestKeywordFunction(IList<IFunction> parameters, IFuturemud gameworld, bool considerInventory, bool considerRoomContents)
 		: base(parameters)
 	{
 		_gameworld = gameworld;
+		ConsiderInventory = considerInventory;
+		ConsiderRoomContents = considerRoomContents;
 	}
 
 	public override FutureProgVariableTypes ReturnType
@@ -22,6 +26,9 @@ internal class BestKeywordFunction : BuiltInFunction
 		get => FutureProgVariableTypes.Text;
 		protected set { }
 	}
+
+	public bool ConsiderInventory { get; set; }
+	public bool ConsiderRoomContents { get; set; }
 
 	public override StatementResult Execute(IVariableSpace variables)
 	{
@@ -37,27 +44,86 @@ internal class BestKeywordFunction : BuiltInFunction
 			return StatementResult.Error;
 		}
 
-		var target = (ICharacter)ParameterFunctions[1].Result;
-		if (target == null)
+		if (ParameterFunctions[1].ReturnType.CompatibleWith(FutureProgVariableTypes.Character))
 		{
-			ErrorMessage = "Target Character was null in BestKeyword function.";
-			return StatementResult.Error;
+			var target = (ICharacter)ParameterFunctions[1].Result;
+			if (target == null)
+			{
+				ErrorMessage = "Target Character was null in BestKeyword function.";
+				return StatementResult.Error;
+			}
+
+			var keywords = target.GetKeywordsFor(source);
+			var targets = source.Location.LayerCharacters(source.RoomLayer).Except(source)
+				.Where(x => x.HasKeywords(keywords, source))
+				.Where(x => source.CanSee(x))
+				.ToList();
+			var index = targets.IndexOf(target);
+			if (index == -1)
+			{
+				ErrorMessage = "Unable to find suitable target in BestKeyword function.";
+				// DEBUG
+				Console.WriteLine("Unable to find suitable target in BestKeyword function.");
+				// END DEBUG
+				return StatementResult.Error;
+			}
+
+			Result = new TextVariable(
+				$"{index + 1}.{keywords.ListToString(separator: ".", conjunction: "", twoItemJoiner: ".")}");
+			return StatementResult.Normal;
 		}
 
-		var keywords = target.GetKeywordsFor(source);
-		var targets = source.Location.Characters.Except(source).Where(x => x.HasKeywords(keywords, source)).ToList();
-		var index = targets.IndexOf(target);
-		if (index == -1)
+		if (ParameterFunctions[1].ReturnType.CompatibleWith(FutureProgVariableTypes.Item))
 		{
-			ErrorMessage = "Unable to find suitable target in BestKeyword function.";
-			// DEBUG
-			Console.WriteLine("Unable to find suitable target in BestKeyword function.");
-			// END DEBUG
-			return StatementResult.Error;
+			var target = (IGameItem)ParameterFunctions[1].Result;
+			if (target == null)
+			{
+				ErrorMessage = "Target Item was null in BestKeyword function.";
+				return StatementResult.Error;
+			}
+
+			var keywords = target.GetKeywordsFor(source);
+			List<IGameItem> targets;
+			if (ConsiderInventory && ConsiderRoomContents)
+			{
+				targets =
+					source.Body.ExternalItems
+					.Concat(source.Location.LayerGameItems(source.RoomLayer))
+					.Where(x => x.HasKeywords(keywords, source))
+					.Where(x => source.CanSee(x))
+					.ToList();
+			}
+			else if (ConsiderInventory)
+			{
+				targets = source.Body.ExternalItems
+					.Where(x => x.HasKeywords(keywords, source))
+					.Where(x => source.CanSee(x))
+					.ToList();
+			}
+			else
+			{
+				targets = source.Location.LayerGameItems(source.RoomLayer)
+					.Where(x => x.HasKeywords(keywords, source))
+					.Where(x => source.CanSee(x))
+					.ToList();
+			}
+			
+			var index = targets.IndexOf(target);
+			if (index == -1)
+			{
+				ErrorMessage = "Unable to find suitable target in BestKeyword function.";
+				// DEBUG
+				Console.WriteLine("Unable to find suitable target in BestKeyword function.");
+				// END DEBUG
+				return StatementResult.Error;
+			}
+
+			Result = new TextVariable(
+				$"{index + 1}.{keywords.ListToString(separator: ".", conjunction: "", twoItemJoiner: ".")}");
+			return StatementResult.Normal;
 		}
 
-		Result = new TextVariable(
-			$"{index + 1}.{keywords.ListToString(separator: ".", conjunction: "", twoItemJoiner: ".")}");
+		Result = new TextVariable("");
 		return StatementResult.Normal;
 	}
 
@@ -71,7 +137,7 @@ internal class BestKeywordFunction : BuiltInFunction
 				FutureProgVariableTypes.Character
 			},
 			(pars, gameworld) =>
-				new BestKeywordFunction(pars, gameworld),
+				new BestKeywordFunction(pars, gameworld, false, false),
 			new List<string> { "source", "target" },
 			new List<string>
 			{
@@ -79,6 +145,66 @@ internal class BestKeywordFunction : BuiltInFunction
 				"The target whose keyword you want to identify"
 			},
 			"This function allows you to determine what the 'best' keyword is for a target from a source. In this case, best implies a unique keyword that will allow the source to target the target through all regular commands.",
+			"Utilities",
+			FutureProgVariableTypes.Text
+		));
+
+		FutureProg.RegisterBuiltInFunctionCompiler(new FunctionCompilerInformation(
+			"bestkeyword",
+			new[]
+			{
+				FutureProgVariableTypes.Character,
+				FutureProgVariableTypes.Item
+			},
+			(pars, gameworld) =>
+				new BestKeywordFunction(pars, gameworld, true, true),
+			new List<string> { "source", "target" },
+			new List<string>
+			{
+				"The source for whom you want to identify the keyword",
+				"The target whose keyword you want to identify"
+			},
+			"This function allows you to determine what the 'best' keyword is for a target from a source. In this case, best implies a unique keyword that will allow the source to target the target through all regular commands. This version of the command considers both the player's inventory and the room contents.",
+			"Utilities",
+			FutureProgVariableTypes.Text
+		));
+
+		FutureProg.RegisterBuiltInFunctionCompiler(new FunctionCompilerInformation(
+			"bestkeywordinventory",
+			new[]
+			{
+				FutureProgVariableTypes.Character,
+				FutureProgVariableTypes.Item
+			},
+			(pars, gameworld) =>
+				new BestKeywordFunction(pars, gameworld, true, false),
+			new List<string> { "source", "target" },
+			new List<string>
+			{
+				"The source for whom you want to identify the keyword",
+				"The target whose keyword you want to identify"
+			},
+			"This function allows you to determine what the 'best' keyword is for a target from a source. In this case, best implies a unique keyword that will allow the source to target the target through all regular commands. This version of the command considers only the player's inventory and not the room contents.",
+			"Utilities",
+			FutureProgVariableTypes.Text
+		));
+
+		FutureProg.RegisterBuiltInFunctionCompiler(new FunctionCompilerInformation(
+			"bestkeywordroom",
+			new[]
+			{
+				FutureProgVariableTypes.Character,
+				FutureProgVariableTypes.Item
+			},
+			(pars, gameworld) =>
+				new BestKeywordFunction(pars, gameworld, false, true),
+			new List<string> { "source", "target" },
+			new List<string>
+			{
+				"The source for whom you want to identify the keyword",
+				"The target whose keyword you want to identify"
+			},
+			"This function allows you to determine what the 'best' keyword is for a target from a source. In this case, best implies a unique keyword that will allow the source to target the target through all regular commands. This version of the command considers only the room contents and not the player's inventory.",
 			"Utilities",
 			FutureProgVariableTypes.Text
 		));

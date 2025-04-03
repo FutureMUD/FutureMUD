5ff964f9d1c5a19847e75c902952ee73c08dfbc7
Luke Cunningham <luke.russell.cunningham@gmail.com>
2025-04-03 17:46:40 +1100

    Fixed multiple delimiters in emotes

--- MudSharpCore/PerceptionEngine/Parsers/EmoteTokens.cs
+++ MudSharpCore/PerceptionEngine/Parsers/EmoteTokens.cs
@@ -43,7 +43,7 @@ public partial class Emote
 	///     !0 - man / you
 	///     !0's man's / your
 	/// </summary>
-	private static readonly Regex InternalTokenRegex = new(@"(?<=([!.?:] |^){0,1})([!$&#%])(\d+)('s){0,1}",
+	private static readonly Regex InternalTokenRegex = new(@"(?<=([!.?:] |^){0,1})(?<!!!)(?<!\$)(?<!&)(?<!#)(?<!%)([!$&#%])(?!\1)(\d+)(?:'s)?",
 		RegexOptions.Multiline);
 
 	/// <summary>

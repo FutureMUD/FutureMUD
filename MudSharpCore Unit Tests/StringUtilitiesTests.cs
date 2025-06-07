72a110bed145ea6b4b9c817461dd8407ad75bbd0
Luke Cunningham <luke.russell.cunningham@gmail.com>
2025-06-07 12:39:54 +1000

    Expand StringUtilities test coverage

--- MudSharpCore Unit Tests/StringUtilitiesTests.cs
+++ MudSharpCore Unit Tests/StringUtilitiesTests.cs
@@ -28,4 +28,48 @@ public class StringUtilitiesTests
         var coloured = "Test #4blue#0 text".SubstituteANSIColour();
         Assert.AreEqual("Test blue text", coloured.StripANSIColour());
     }
+
+    [TestMethod]
+    public void SubstituteANSIColour_EscapedHash()
+    {
+        var input = "Value ##4 should stay";
+        var expected = "Value #4 should stay";
+        Assert.AreEqual(expected, input.SubstituteANSIColour());
+    }
+
+    [TestMethod]
+    public void SubstituteANSIColour_InvalidCode_Unchanged()
+    {
+        var input = "Unknown #z tag";
+        Assert.AreEqual(input, input.SubstituteANSIColour());
+    }
+
+    [TestMethod]
+    public void SubstituteANSIColour_NullInput_ReturnsEmpty()
+    {
+        string input = null;
+        Assert.AreEqual(string.Empty, input.SubstituteANSIColour());
+    }
+
+    [TestMethod]
+    public void SubstituteANSIColour_MultipleCodes()
+    {
+        var input = "Red #1r#0 and blue #4b#0.";
+        var expected = $"Red {Telnet.Red}r{Telnet.RESETALL} and blue {Telnet.Blue}b{Telnet.RESETALL}.";
+        Assert.AreEqual(expected, input.SubstituteANSIColour());
+    }
+
+    [TestMethod]
+    public void StripANSIColour_NoSequences_ReturnsOriginal()
+    {
+        var input = "Plain text";
+        Assert.AreEqual(input, input.StripANSIColour());
+    }
+
+    [TestMethod]
+    public void StripANSIColour_ComplexString()
+    {
+        var coloured = "Mix #1red#0 and #4blue#0".SubstituteANSIColour();
+        Assert.AreEqual("Mix red and blue", coloured.StripANSIColour());
+    }
 }

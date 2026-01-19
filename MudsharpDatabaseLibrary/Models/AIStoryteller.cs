c15247e90a50d1373ddd2dd28d5e2bc2a58f5f11
Luke Cunningham <luke.russell.cunningham@gmail.com>
2026-01-20 09:05:09 +1100

    WIP AI Storyteller

---
+++ MudsharpDatabaseLibrary/Models/AIStoryteller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MudSharp.Models;
+
+public class AIStoryteller
+{
+	public long Id { get; set; }
+	public string Name { get; set; }
+	public string Description { get; set; }
+	public string Model { get; set; }
+	public string SystemPrompt { get; set; }
+}

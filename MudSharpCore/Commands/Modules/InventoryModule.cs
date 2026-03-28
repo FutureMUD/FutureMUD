96056ccc4376df056733697d13dd94a29aac675a
Luke Cunningham <luke.russell.cunningham@gmail.com>
2026-03-28 22:57:05 +1100

    Validate strip consent before starting accepted undress actions

--- MudSharpCore/Commands/Modules/InventoryModule.cs
+++ MudSharpCore/Commands/Modules/InventoryModule.cs
@@ -2482,7 +2482,36 @@ The possible syntaxes for this command are:
 			target.Send(
 				$"You must type {"accept".Colour(Telnet.Yellow)} to permit them to do so, or {"decline".Colour(Telnet.Yellow)} to decline.");
 			target.AddEffect(new Accept(target, new GenericProposal(
-				perc => { BeginStrip(true); },
+				perc =>
+				{
+					if (!target.ColocatedWith(actor))
+					{
+						target.Send("You are no longer in the same location as the person who wanted to strip you.");
+						return;
+					}
+
+					if (target.Combat != null && target.MeleeRange)
+					{
+						target.Send("You can't be willingly stripped while you are in melee combat!");
+						return;
+					}
+
+					if (actor.Combat != null && actor.MeleeRange)
+					{
+						target.Send(
+							$"{actor.HowSeen(target, true)} is no longer capable of stripping you as they are in melee combat.");
+						return;
+					}
+
+					if (!actor.State.IsAble())
+					{
+						target.Send(
+							$"{actor.HowSeen(target, true)} is no longer capable of stripping you as they are {actor.State.Describe()}.");
+						return;
+					}
+
+					BeginStrip(true);
+				},
 				perc =>
 				{
 					target.OutputHandler.Handle(

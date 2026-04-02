9126cc7704c118e9a1efdeed4f44381b10886bc3
Luke Cunningham <luke.russell.cunningham@gmail.com>
2026-04-03 10:49:45 +1100

    Fixing a startup bug

--- MudSharpCore/NPC/AI/ScavengeAI.cs
+++ MudSharpCore/NPC/AI/ScavengeAI.cs
@@ -221,7 +221,7 @@ public class ScavengeAI : ArtificialIntelligenceBase
 
 	internal static bool IsValidOnScavengeProg(IFutureProg prog)
 	{
-		return prog.ReturnType == ProgVariableTypes.Void &&
+		return 
 			prog.MatchesParameters(new List<ProgVariableTypes>
 		       {
 			       ProgVariableTypes.Character,

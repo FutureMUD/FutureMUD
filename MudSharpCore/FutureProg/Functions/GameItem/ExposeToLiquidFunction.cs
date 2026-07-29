cabf0c93c33218ef68f5d139b82aa29309a9a5a5
Luke Cunningham <luke.russell.cunningham@gmail.com>
2026-07-30 09:46:03 +1000

    Expand FutureProg liquid exposure targets

--- MudSharpCore/FutureProg/Functions/GameItem/ExposeToLiquidFunction.cs
+++ MudSharpCore/FutureProg/Functions/GameItem/ExposeToLiquidFunction.cs
@@ -1,6 +1,7 @@
 #nullable enable
 
 using MudSharp.Form.Material;
+using MudSharp.Framework.Units;
 using MudSharp.FutureProg.Variables;
 using MudSharp.GameItems;
 
@@ -36,7 +37,7 @@ internal class ExposeToLiquidFunction : BuiltInFunction
 		}
 
 		var liquidId = (long)((decimal?)ParameterFunctions[1].Result?.GetObject ?? 0.0M);
-		var volume = (double)((decimal?)ParameterFunctions[2].Result?.GetObject ?? 0.0M);
+		var volume = GetVolume();
 		var liquid = _gameworld.Liquids.Get(liquidId);
 		if (liquid is null || volume <= 0.0)
 		{
@@ -52,6 +53,18 @@ internal class ExposeToLiquidFunction : BuiltInFunction
 		return StatementResult.Normal;
 	}
 
+	private double GetVolume()
+	{
+		if (ParameterFunctions[2].ReturnType.CompatibleWith(ProgVariableTypes.Number))
+		{
+			return (double)((decimal?)ParameterFunctions[2].Result?.GetObject ?? 0.0M);
+		}
+
+		var text = ParameterFunctions[2].Result?.GetObject?.ToString() ?? string.Empty;
+		var volume = _gameworld.UnitManager.GetBaseUnits(text, UnitType.FluidVolume, out var success);
+		return success ? volume : 0.0;
+	}
+
 	public static void RegisterFunctionCompiler()
 	{
 		FutureProg.RegisterBuiltInFunctionCompiler(new FunctionCompilerInformation(
@@ -65,6 +78,20 @@ internal class ExposeToLiquidFunction : BuiltInFunction
 				"The volume of liquid to apply in base fluid units."
 			],
 			"Exposes an item's upper surface to a volume of liquid, using the normal absorption, contamination, drying and residue rules. Returns false for a null item, unknown liquid or non-positive volume.",
+			"Items",
+			ProgVariableTypes.Boolean));
+
+		FutureProg.RegisterBuiltInFunctionCompiler(new FunctionCompilerInformation(
+			"exposetoliquid",
+			[ProgVariableTypes.Item, ProgVariableTypes.Number, ProgVariableTypes.Text],
+			(parameters, gameworld) => new ExposeToLiquidFunction(parameters, gameworld),
+			["item", "liquidId", "volume"],
+			[
+				"The item whose surface should be exposed to the liquid.",
+				"The ID of the liquid to apply.",
+				"The volume of liquid to apply, including its fluid-volume unit."
+			],
+			"Exposes an item's upper surface to a text-specified volume of liquid, using the normal absorption, contamination, drying and residue rules. Returns false for a null item, unknown liquid, invalid unit expression or non-positive volume.",
 			"Items",
 			ProgVariableTypes.Boolean));
 	}

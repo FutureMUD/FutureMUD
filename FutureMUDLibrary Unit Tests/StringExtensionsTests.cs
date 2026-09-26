20a4b85fa44e1db6ca0110f01a85d8afa26dcfb3
Luke Cunningham <luke.russell.cunningham@gmail.com>
2026-09-26 22:17:18 +1000

    Fix title casing around internal apostrophes

--- FutureMUDLibrary Unit Tests/StringExtensionsTests.cs
+++ FutureMUDLibrary Unit Tests/StringExtensionsTests.cs
@@ -151,7 +151,10 @@ public class StringExtensionsTests
         Assert.AreEqual("Offset Two", "Offset Two".TitleCase());
         Assert.AreEqual("ZZZ Pilot Weather", "ZZZ Pilot Weather".TitleCase());
         Assert.AreEqual("Earth's Sky", "earth's sky".TitleCase());
-		Assert.AreEqual("He said, breathlessly, \"Hello.\"", "he said, breathlessly, \"hello.\"".TitleCase());
+		Assert.AreEqual("Earth’s Sky", "earth’s sky".TitleCase());
+		Assert.AreEqual("'Earth's Sky'", "'earth's sky'".TitleCase());
+		Assert.AreEqual("‘Earth’s Sky’", "‘earth’s sky’".TitleCase());
+		Assert.AreEqual("He Said, Breathlessly, \"Hello.\"", "he said, breathlessly, \"hello.\"".TitleCase());
 	}
 
     [TestMethod]
